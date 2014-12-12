3284dafb707c7062735b1273ac16e3b3e9619763
Afreen <simu_ete@yahoo.com>
2014-12-12 10:00:38 +0600

    array_flip() example

--- create/Exercise9.php
+++ create/Exercise9.php
@@ -4,12 +4,14 @@ function byVal($arg) {
     echo 'As passed     : ', var_export(func_get_args()), PHP_EOL;
     $arg = 'baz';
     echo 'After change  : ', var_export(func_get_args()), PHP_EOL;
+    echo "<br>";
 }
 
 function byRef(&$arg) {
     echo 'As passed     : ', var_export(func_get_args()), PHP_EOL;
     $arg = 'baz';
     echo 'After change  : ', var_export(func_get_args()), PHP_EOL;
+    echo "<br>";
 }
 
 $arg = 'bar';
