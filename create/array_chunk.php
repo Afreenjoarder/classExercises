a157c24ae639827c75c4c779969cce221e0a2b53
tanjina <tanjina_tushar@yahoo.com>
2014-12-15 11:37:08 +0600

    array_chunk() example

--- create/array_chunk.php
+++ create/array_chunk.php
@@ -1,6 +1,6 @@
 
 <?php
-$input_array = array('a', 'b', 'c', 'd', 'e');
+$input_array = array('a', 'b', 'c', 'd', 'f');
 print_r(array_chunk($input_array, 2));
 
 print_r(array_chunk($input_array, 2, true));
